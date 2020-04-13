6242bd15c13317166020a115fb308116d369a862
Léa commaret <lea.commaret@edu.ece.fr>
2020-04-13 14:49:13 +0200

    a

--- compte.php
+++ compte.php
@@ -2,7 +2,7 @@
 
 
 
-$ID=isset($_POST["ID"])? $_POST["ID"] : ""; //généré par phpmyadmin 
+$ID=isset($_POST["ID"])? $_POST["ID"] : ""; //généré par phpmyadmin   //partie inscription
 $Nom= isset($_POST["Nom"])? $_POST["Nom"] : "";
 $Mail= isset($_POST["Mail"])? $_POST["Mail"] : "";
 $Telephone= isset($_POST["Telephone"])? $_POST["Telephone"] : "";
@@ -10,10 +10,12 @@ $Role = isset($_POST["Role"])? $_POST["Role"] : "";
 $Mdp1= isset($_POST["Mdp1"])? $_POST["Mdp1"] : "";
 $Mdp2= isset($_POST["Mdp2"])? $_POST["Mdp2"] : "";
 
+ 
+$Pseudo= isset($_POST["Pseudo"])? $_POST["Pseudo"] : ""; //partie connexion 
+$Mdp3= isset($_POST["Mdp3"])? $_POST["Mdp3"] : "";
 
 
-
-if (isset($_POST['button1'])) 
+if (isset($_POST['button1'])) //inscription
 
    {
 	 if($Nom!=""&&$Mail!=""&&$Telephone!=""&&$Role!=""&&$Mdp1!=""&&$Mdp2!="")
@@ -29,6 +31,17 @@ if (isset($_POST['button1']))
 }
         
 else {echo "Probleme";}
+
+
+if (isset($_POST['button2'])) //connexion
+{
+    if($Pseudo!=""&&$Mdp3!="")
+    { 
+
+    	
+
+    }else echo "Veuillez remplir tout les champs svp";
+}
 
 
 
