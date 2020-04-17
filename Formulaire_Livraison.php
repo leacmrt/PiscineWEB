980c86e1d7714b91549d707f5a2fcc3853eacf26
Léa commaret <lea.commaret@edu.ece.fr>
2020-04-17 12:35:27 +0200

    formulaire + sql de données

---
+++ Formulaire_Livraison.php
@@ -0,0 +1,194 @@
+<?php
+
+$database = "testpiscine";//connectez-vous dans votre BDD//Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+
+$Adresse1 = isset($_POST["Adresse1"])? $_POST["Adresse1"] : ""; 
+$Adresse2 = isset($_POST["Adresse2"])? $_POST["Adresse2"] : ""; 
+$Ville= isset($_POST["Ville"])? $_POST["Ville"] : ""; 
+$CodeP = isset($_POST["CodeP"])? $_POST["CodeP"] : ""; 
+$Pays = isset($_POST["Pays"])? $_POST["Pays"] : ""; 
+$CarteNom = isset($_POST["CarteNom"])? $_POST["CarteNom"] : ""; 
+$CarteNum = isset($_POST["CarteNum"])? $_POST["CarteNum"] : ""; 
+$CVC= isset($_POST["CVC"])? $_POST["CVC"] : ""; 
+$Expiration= isset($_POST["Expiration"])? $_POST["Expiration"] : ""; 
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Données de livraisons</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-color:#c7ecee">
+
+
+<nav class="navbar navbar-inverse" style="background-color:#22a6b3">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><img src="logo4.png" width="30" height="30"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#"  style="color:#ecf0f1"><b><font size = "+1">Home</font></b></a></li>
+        <li><a href="#" style="color:#ecf0f1"><b><font size = "+1">Catégories</font></b></a></li>
+        <li><a href="#" style="color:#ecf0f1"><b><font size = "+1">Achat</font></b></a></li>
+        <li><a href="#" style="color:#ecf0f1"><b><font size = "+1">Vendre</font></b></a></li>
+        <li><a href="#" style="color:#ecf0f1"><b><font size = "+1">Admin</font></b></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="develop1.html" style="color:#ecf0f1"><span class="glyphicon glyphicon-user"></span><b><font size = "+1"> Compte</font></b></a></li>
+        <li><a href="panier.html" style="color:#ecf0f1"><span class="glyphicon glyphicon-shopping-cart"></span><b><font size = "+1"> Panier</font></b></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+ <center>
+      <img src="logo2.png" width="500" height="150">
+    </center>
+<br>
+
+
+<div class="container">
+<center>
+   
+  <div style="width: 700px; height: 1000px">
+  <div style=" border:5px solid; border-color: #22a6b3; padding-left: 30px;padding-right: 30px;background-color:#ffffff">
+
+    <div class="text-center border border-light p-5">
+       <h3> Vos informations de payment </h3>
+
+         <form class="form-horizontal" action="" method="post">
+             <br>
+             <div class='form-row'>
+
+              <div class='col-xs-12'>
+                <label class='control-label'>Adresse </label>
+                <input class='form-control' name="Adresse1"size='4' type='text' placeholder='Ligne 1'>
+                <input class='form-control' name="Adresse2"size='4' type='text' placeholder='Ligne 2'>
+              </div><br><br><br><br>
+              
+
+              <div class='form-row'>
+              <div class='col-xs-4'>
+                <label class='control-label'>Ville</label>
+                <input  class='form-control' name='Ville' placeholder=' size='4' type='text'>
+              </div>
+
+              <div class='col-xs-4'>
+                <label class='control-label'>Code Postal</label>
+                <input  class='form-control' name='CodeP' placeholder='' size='4' type='text'>
+              </div>
+
+              <div class='col-xs-4'>
+                <label class='control-label'>Pays</label>
+                <input class='form-control' name='Pays' placeholder='' size='2' type='text'>
+              </div>
+              <br><br><br><br><br>
+            </div>
+
+              <div class='col-xs-12'>
+                <label class='control-label'>Nom sur la carte </label>
+                <input class='form-control' name='CarteNom' size='4' type='text'>
+              </div><br><br><br>
+            </div>
+            <div class='form-row'>
+              <div class='col-xs-12 '>
+                <label class='control-label'>Numéro de carte</label>
+                <input class='form-control' name='CarteNum' size='20' type='text'>
+              </div><br><br><br>
+            </div>
+            <div class='form-row'>
+              <div class='col-xs-4'>
+                <label class='control-label'>CVC</label>
+                <input  class='form-control' name='CVC' placeholder='ex. 311' size='4' type='text'>
+              </div>
+              <div class='col-xs-8'>
+                <label class='control-label'>Expiration</label>
+                <input class='form-control' name='Expiration' placeholder='JJ-MM-AAAA' size='2' type='text'>
+              </div>
+              <br><br><br><br><br>
+            </div>
+            
+            <div class='form-row'>
+              <br>
+              <div class='col-md-12 form-group'>
+                <button class='form-control btn btn-primary submit-button' name="button1" type='submit'>Proceder au payement »</button> 
+              </div><br><br>
+            </div>
+            <br><br>
+      </form>
+    </div>
+    
+    </div>
+  </div>
+  </center>
+  </div>
+
+   
+
+
+<footer class="container-fluid text-center">
+  <p><br><br>©EBAY-ECE 2020</p>  
+</footer>
+
+</body>
+</html>
+
+
+
+<?php
+
+       if (isset($_POST['button1']))//si le formulaire s'envoie
+       {
+           
+            if($Adresse1!=""&&$Ville!=""&&$CodeP!=""&&$Pays!=""&&$CarteNom!=""&&$CarteNum!=""&&$CVC!=""&&$Expiration!="")//si tout les champs sont remplis (sauf adresse 2 on s'en fou) 
+              { 
+                   if($db_found)
+                   {
+                          $sql = "SELECT * FROM donnees";
+                           if ($CarteNum != "") {//on cherche le livre avec les paramètres titre et auteur
+                          $sql .= " WHERE CarteNum LIKE '%$CarteNum%'";
+                           if ($CarteNom != "") {
+                             $sql .= " AND CarteNom LIKE '%$CarteNom%'";}}
+                              $result = mysqli_query($db_handle, $sql);//regarder s'il y a de résultat
+
+                            if(mysqli_num_rows($result) != 0) 
+                          {echo "<script> window.alert(\"Le compte existe . <br>\")";}
+                          else
+                          {
+                               $sql= "INSERT INTO donnees(Adresse1, Adresse2, Ville, CodeP, Pays, CarteNom,CarteNum,CVC,Expiration,ID_Vendeur)
+                               VALUES('$Adresse1', '$Adresse2', '$Ville', '$CodeP','$Pays','$CarteNom','$CarteNum', '$CVC','$Expiration','0')";
+                              $result = mysqli_query($db_handle, $sql);
+                              echo "<script> window.alert(\"Vos données sont bien enregistrées\"); history.back(); </script>";
+
+
+                          }
+
+                  }
+              }else echo " <script> alert(\"Veuillez remplir tout les champs!\") </script>";
+
+       }
+ 
+?>
---
+++ donnees.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 17 avr. 2020 à 10:34
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `testpiscine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `donnees`
+--
+
+DROP TABLE IF EXISTS `donnees`;
+CREATE TABLE IF NOT EXISTS `donnees` (
+  `Adresse1` text NOT NULL,
+  `Adresse2` text NOT NULL,
+  `Ville` text NOT NULL,
+  `CodeP` text NOT NULL,
+  `Pays` text NOT NULL,
+  `CarteNom` text NOT NULL,
+  `CarteNum` text NOT NULL,
+  `CVC` text NOT NULL,
+  `Expiration` text NOT NULL,
+  `ID_Vendeur` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
