be210d55927423434178c67103c628bff44aa333
Wichawee <wichawee.saycocie@edu.ece.fr>
2020-04-20 13:05:13 +0200

    Changement nom onglet navigateur
    Changement des noms pour coller à la page.

--- profil.php
+++ profil.php
@@ -23,7 +23,7 @@ $db_found = mysqli_select_db($db_handle, $database);
 <!DOCTYPE html>
     <html lang="en">
     <head>
-      <title>Bootstrap Example</title>
+      <title>Mon compte</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
