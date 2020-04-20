fb59aef7561f0ec3d39a4b41b18abd6ddfe34534
Léa commaret <lea.commaret@edu.ece.fr>
2020-04-20 16:08:22 +0200

    bug en moins

--- commande4.php
+++ commande4.php
@@ -10,7 +10,7 @@ $t=$_SESSION['ID_vendeur'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Commande après enchere</title>
+  <title> Commande après enchere </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
