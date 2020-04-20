bcdaaa2e2c48d13d6797a5b75be5b64cf82934e2
Wichawee <wichawee.saycocie@edu.ece.fr>
2020-04-20 15:05:23 +0200

    ajout titre

--- article.php
+++ article.php
@@ -22,7 +22,7 @@ $ModifMe1 = isset($_POST["ModifMe1"])? $_POST["ModifMe1"] : "";
 <html lang="en">
 
 <head>
-
+  <title>Article</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
